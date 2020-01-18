9886e3d558f8be86fb7449f83de17bf7888a3b64
mikita_zdanovich@epam.com <47656078+MikZdanovich@users.noreply.github.com>
2020-01-19 00:46:58 +0300

    nginx configured, but didnt' work with restplus swagger up, do not merge this branch in master

--- backend/app/main/__init__.py
+++ backend/app/main/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-
+from werkzeug.middleware.proxy_fix import ProxyFix
 from app.main.config import config_by_name
 
 flask_bcrypt = Bcrypt()
@@ -11,6 +11,7 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.config.from_object(config_by_name[config_name])
 
     db.init_app(app)
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,8 +2,11 @@ import os
 
 from app import blueprint
 from app.main import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.wsgi_app = ProxyFix(app.wsgi_app)
 app.register_blueprint(blueprint)
 
 app.app_context().push()
