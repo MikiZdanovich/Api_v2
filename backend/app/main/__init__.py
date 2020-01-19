60c51ef03d5df47b20e73096e21bf665a8d55d3d
mikita_zdanovich@epam.com <47656078+MikZdanovich@users.noreply.github.com>
2020-01-19 16:04:49 +0300

    rabbit:managment service plus celery app instance

--- backend/app/main/__init__.py
+++ backend/app/main/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-
 from app.main.config import config_by_name
 
 flask_bcrypt = Bcrypt()
@@ -17,3 +16,4 @@ def create_app(config_name):
     flask_bcrypt.init_app(app)
 
     return app
+
