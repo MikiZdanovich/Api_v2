ca1f8fe6bfa4fb67ca17df282a91fc68e4bc59e4
mikita_zdanovich@epam.com <47656078+MikZdanovich@users.noreply.github.com>
2020-02-13 11:31:30 +0300

    unit tests for get_repositories_service.py

--- backend/app/test/test_get_repositories_service.py
+++ backend/app/test/test_get_repositories_service.py
@@ -38,41 +38,52 @@ class TestParseResponse:
             parse_response(response)
 
 
-class MockResponseOk:
-    def __init__(self):
-        self.status_code = 200
-
-    @staticmethod
-    def json():
-        return [{"Test": "test"}, {"name": ["repo1"]}]
-
-    @pytest.fixture
-    def mock_response(self, monkeypatch):
-        """Requests.get() mocked to return {"status": 200, "data": [{"Test": "test"}, {"name": ["repo1"]}]."""
-
-        def mock_get(*args, **kwargs):
-            return MockResponseOk()
-
-        monkeypatch.setattr(requests, "get", mock_get)
-
-
 class TestGetDataFromGit:
     """test_data_from_git_api accept Git username, make get request
     to Git api, and return Dict with status_code and List of user repositories"""
 
-    @staticmethod
-    def json():
-        return [{"Test": "test"}, {"name": ["repo1"]}]
+    def test_get_data_from_git_api_succed(self, monkeypatch):
+        """
+            GIVEN a monkeypatched version of requests.get()
+            WHEN the HTTP response is set to successful
+            THEN check the HTTP response
+            """
 
-    @pytest.fixture
-    def mock_response(self, monkeypatch):
-        """Requests.get() mocked to return {"status": 200, "data": [{"Test": "test"}, {"name": ["repo1"]}]."""
+        class MockResponseOk:
+            def __init__(self):
+                self.status_code = 200
+
+            @staticmethod
+            def json():
+                return [{"Test": "test"}, {"name": ["repo1"]}]
 
         def mock_get(*args, **kwargs):
             return MockResponseOk()
 
         monkeypatch.setattr(requests, "get", mock_get)
 
-    def test_get_data_from_git_api(self, mock_response):
-        setup = {"status": 200, "data": [{"Test": "test"}, {"name": ["repo1"]}]}
-        assert get_data_from_git_api("test") == setup
+        expected = {"status": 200, "data": [{"Test": "test"}, {"name": ["repo1"]}]}
+        assert get_data_from_git_api("test") == expected
+
+    def test_get_data_from_git_api_failure(self, monkeypatch):
+        """
+            GIVEN a monkeypatched version of requests.get()
+            WHEN the HTTP response is set to failed
+            THEN check the HTTP response
+            """
+
+        class MockResponseFalse:
+            def __init__(self):
+                self.status_code = 404
+
+            @staticmethod
+            def json():
+                return [{"Test": "test"}, {"name": ["repo1"]}]
+
+        def mock_get(*args, **kwargs):
+            return MockResponseFalse()
+
+        monkeypatch.setattr(requests, "get", mock_get)
+
+        expected = {"status": 404, "data": [{"Test": "test"}, {"name": ["repo1"]}]}
+        assert get_data_from_git_api("test") == expected
